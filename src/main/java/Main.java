6660f4511b3b8fdc2443e72bbf29dde0dcd9c798
jakeTheDog <mbivol0002@gmail.com>
2026-04-05 16:52:13 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
          System.out.print("$ ");
+
+         Scanner in = new Scanner(System.in);
+         String command = in.nextLine();
+         System.out.format("%s: command not found", command);
     }
 }
