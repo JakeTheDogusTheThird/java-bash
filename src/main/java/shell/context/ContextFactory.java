e8ad6e55dc60b3bb17d5fcc01a5de88168915fe5
JakeTheDogus <proxysmile@UniqueEmail.com>
2026-04-11 18:29:17 +0300

    refactor: extract os resolution to constant

--- src/main/java/shell/context/ContextFactory.java
+++ src/main/java/shell/context/ContextFactory.java
@@ -1,9 +1,10 @@
 package shell.context;
 
 public class ContextFactory {
+    private static final String OS = System.getProperty("os.name");
+
     public static ShellContext getContext() {
-        String os = System.getProperty("os.name").toLowerCase();
-        return os.contains("win")
+        return OS.contains("win")
                 ? new WindowsShellContext()
                 : new UnixShellContext();
     }
